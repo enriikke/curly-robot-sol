7e80fe3ec8bb7b46572767751639ac4a661c19ee
Enrique Gonzalez <enriikke@gmail.com>
2021-11-07 01:17:08 +0000

    Add and count gifs
    Signed-off-by: GitHub <noreply@github.com>

--- gif-collector/programs/gif-collector/src/lib.rs
+++ gif-collector/programs/gif-collector/src/lib.rs
@@ -13,6 +13,21 @@ pub mod gif_collector {
 
         Ok(())
     }
+
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+
+        // Build the struct.
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *base_account.to_account_info().key,
+        };
+
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }
 }
 
 // Attach certain variables to the StartStuffOff context.
@@ -25,8 +40,21 @@ pub struct Initialize<'info> {
     pub system_program: Program <'info, System>,
 }
 
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
 // Tell Solana what we want to store on this account.
 #[account]
 pub struct BaseAccount {
     pub total_gifs: u64,
+    pub gif_list: Vec<ItemStruct>,
 }
--- gif-collector/tests/gif-collector.js
+++ gif-collector/tests/gif-collector.js
@@ -1,14 +1,43 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
 describe('gif-collector', () => {
   console.log("🚀 Starting test...")
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   it('Is initialized!', async () => {
     const program = anchor.workspace.GifCollector;
-    const tx = await program.rpc.initialize();
+    // Create an account keypair for our program to use.
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    let tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
     console.log("📝 Your transaction signature", tx);
+
+    // Fetch data from the account.
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('👀 GIF Count', account.totalGifs.toString())
+
+    await program.rpc.addGif("https://media.giphy.com/media/8Ry7iAVwKBQpG/giphy.gif", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    // Get the account again to see what changed.
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('👀 GIF Count', account.totalGifs.toString())
+
+    console.log('👀 GIF List', account.gifList)
   });
 });
